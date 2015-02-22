1fe040c60ddd2e5f9759cf6ef18359bf1c7cd87c
Andrew Anderson <andrewloveskitties@gmail.com>
2015-02-22 12:15:17 -0800

    Make kitty smaller :)

--- anim/kitty.lua
+++ anim/kitty.lua
@@ -1,7 +1,7 @@
 require('class.animationtemplate')
 require('util.anim')
 
-local size = {w = 7, h = 5}
+local size = {w = 6, h = 5}
 local quads = create_quads(TEXTURE, {x = 0, y = 0}, size, 13)
 
 local templates = {}
